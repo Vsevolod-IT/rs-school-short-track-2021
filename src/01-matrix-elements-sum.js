3ee07ff9e61bf11966ded5c07bb99f69b52fb66f
Vsevolod-IT <it_vsevolod@yahoo.com>
2021-04-12 23:26:43 +0300

    2 tasks

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,28 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  const countRow = matrix.length;
+  const countCol = matrix[0].length;
+  const tmp = [];
+
+  for (let i = 0; i < countCol; i++) {
+    for (let j = 0; j !== countRow; j++) {
+      tmp.push(matrix[j].pop());
+    }
+    tmp.reverse();
+    for (let k = 0; k !== countRow; k++) {
+      const f = tmp.pop();
+      if (f === 0) {
+        break;
+      } else {
+        sum += f;
+      }
+    }
+    tmp.length = 0;
+  }
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,26 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  let res = 0;
+  let arr1 = s1.split('');
+  const set1 = new Set(arr1);
+  arr1 = Array.from(set1);
+  let arr2 = s2.split('');
+  const set2 = new Set(arr2);
+  arr2 = Array.from(set2);
+  const intersection = arr1.filter((x) => arr2.includes(x));
+  const dict = new Map();
+
+  intersection.forEach((o) => {
+    dict.set(o, 0);
+  });
+  dict.forEach((value, key) => {
+    const count1 = s1.split(key).length - 1;
+    const count2 = s2.split(key).length - 1;
+    res = count1 >= count2 ? res += count2 : res += count1;
+  });
+  return res;
 }
 
 module.exports = getCommonCharacterCount;
